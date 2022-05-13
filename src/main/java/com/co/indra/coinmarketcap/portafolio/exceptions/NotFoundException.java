ad7430f81bf4aa52e5bf9f71bd4025174bc0ced0
deisygarcia188 <carolinagarciam10@gmail.com>
2022-05-13 13:40:10 -0500

    Deisy: Se realiza el metodo para crear portafolio y listar portafolios

--- src/main/java/com/co/indra/coinmarketcap/portafolio/exceptions/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.co.indra.coinmarketcap.portafolio.exceptions;
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message) {
-        super(message);
-    }
-
-}
--- src/main/resources/db/migration/V1.0__create_portafolio_table.sql
+++
@@ -1,6 +0,0 @@
-create table public.tbl_portafolios (
-    id_portafolio serial primary key,
-    mail_user varchar(255) NOT NULL,
-    name_portafolio varchar(255) NOT NULL,
-    balance_portafolio numeric NOT NULL
-)
